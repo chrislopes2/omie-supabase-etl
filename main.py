2993a0ba20b97fcd547440f6a5f926ae45f9ee97
Cristhofer Lopes Maciel <cristhofer.maciel@grupostudio.local>
2026-05-22 15:56:55 -0300

    feat: adiciona extracao de departamentos em contas a receber

--- main.py
+++ main.py
@@ -89,7 +89,8 @@ def puxar_contas_receber(empresa_config):
                     "valor_documento": conta.get("valor_documento"),
                     "status_titulo": conta.get("status_titulo"),
                     "codigo_categoria": conta.get("codigo_categoria"),
-                    "categorias": conta.get("categorias")
+                    "categorias": conta.get("categorias"),
+                    "departamentos": conta.get("departamentos")
                 }
                 todos_registros.append(registro)
             pagina += 1
