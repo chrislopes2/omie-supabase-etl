dbaaafc1a5bc8d61bf73a8d1e2c9594598f8db9c
Cristhofer Lopes Maciel <cristhofer.maciel@grupostudio.local>
2026-06-29 17:43:07 -0300

    chore: swap STUDIO FISCAL credentials to old valid key and cleaned cnpj

--- sync_categorias.py
+++ sync_categorias.py
@@ -17,7 +17,7 @@ if not SUPABASE_URL or not SUPABASE_KEY:
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 TODAS_EMPRESAS = [
-    {"empresa": "STUDIO FISCAL", "app_key": "3834642953322", "app_secret": "d2ee7fbd30c0065a4c95dd7c4e511417", "cnpj": "40211867000146"}
+    {"empresa": "STUDIO FISCAL", "app_key": "2900565432765", "app_secret": "d409267d73b8886896ffe001c7c93c43", "cnpj": "08865854000142"}
 ]
 
 def formatar_registro(cat, empresa_config):
